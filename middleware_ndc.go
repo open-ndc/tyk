57b31f06707df4166696e6264b188955f4c05476
Matias Insaurralde <matias@insaurral.de>
2016-01-26 11:37:00 -0300

    Basic structs and content type filter

--- middleware_ndc.go
+++ middleware_ndc.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"io"
 	"net/http"
-	"strings"
 	"fmt"
  "bytes"
 )
 
-// NDCMiddleware is a caching middleware that will pull data from Redis instead of the upstream proxy
+// NDCMiddleware is a middleware to perform analytics based on the request body / message
+
+const (
+	// TODO: support a range of content types
+	acceptedContentType string = "application/xml"
+)
+
 type NDCMiddleware struct {
 	*TykMiddleware
 	CacheStore StorageHandler
@@ -20,40 +22,59 @@ type NDCMiddleware struct {
 type NDCMiddlewareConfig struct {
 }
 
+// Custom structs (these should be in a separate place later):
+
+type AirShoppingRQType struct {
+	PointOfSale pointOfSaleType
+}
+
+type pointOfSaleType struct {
+	Location locationType
+}
+
+type locationType struct {
+	CountryCode string
+	CityCode    string
+}
+
 // New lets you do any initialisations for the object can be done here
+
 func (m *NDCMiddleware) New() {
-        fmt.Println( "INITIALIZE! ")
+	fmt.Println( "*** NDCMiddleware init" )
+	fmt.Println( "*** NDCMiddleware, acceptedContentType:" + acceptedContentType)
 	m.sh = SuccessHandler{m.TykMiddleware}
 }
 
 // GetConfig retrieves the configuration from the API config - we user mapstructure for this for simplicity
+
 func (m *NDCMiddleware) GetConfig() (interface{}, error) {
 	var thisModuleConfig NDCMiddlewareConfig
 	return thisModuleConfig, nil
 }
 
-func (m NDCMiddleware) CreateCheckSum(req *http.Request, keyName string) string {
-	h := md5.New()
-	toEncode := strings.Join([]string{req.Method, req.URL.String()}, "-")
-	log.Debug("Cache encoding: ", toEncode)
-	io.WriteString(h, toEncode)
-	reqChecksum := hex.EncodeToString(h.Sum(nil))
-
-	cacheKey := m.Spec.APIDefinition.APIID + keyName + reqChecksum
-
-	return cacheKey
-}
 // ProcessRequest will run any checks on the request on the way through the system, return an error to have the chain fail
+
 func (m *NDCMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Request, configuration interface{}) (error, int) {
 	fmt.Println( "*** NDCMiddleware:ProcessRequest")
 
 	var copiedRequest *http.Request = CopyHttpRequest( r )
-	// copiedRequest = CopyHttpRequest(r)
-
+	
 	buf := new(bytes.Buffer)
 	buf.ReadFrom( copiedRequest.Body )
 
-	fmt.Println( buf.String() )
+	// fmt.Println( buf.String() )
+
+	// TODO: handle nil contentType
+	var contentType string = r.Header[ "Content-Type" ][0]
+
+	if( acceptedContentType != contentType ) {
+		fmt.Println( "Not tracking this request")
+		return nil, 200
+	}
+
+	fmt.Println( "Tracking this request")
 
 	return nil, 200
+
+
 }
